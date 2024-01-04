43874c1378ce79abb193fffb51e9f43465903797
Atabey Qhan <atabeyqhan@gmail.com>
2024-01-04 14:17:03 +0800

    basic3

--- pages/student/includes/footer.php
+++ pages/student/includes/footer.php
@@ -1,6 +1,5 @@
 <footer class="main-footer text-center">
   <strong>Hak Cipta &copy; 2024<a href="#"> RN Technologies Sdn. Bhd.</a></strong>
-  All rights reserved.
   <div class="float-right d-none d-sm-inline-block">
     <b>Version</b> 1.0
   </div>
